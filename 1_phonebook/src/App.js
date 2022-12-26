18b6797360eeba6b957e60e5b9587bca5c274567
justin_sarg <souporcerealemail@gmail.com>
2022-12-26 11:51:31 -0500

    2.11

--- 1_phonebook/src/App.js
+++ 1_phonebook/src/App.js
@@ -1,14 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    { name: 'Loading', number: '...', id: -1 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -38,6 +36,16 @@ const App = () => {
     setNewSearch(event.target.value)
   }
 
+  useEffect(() => {
+    //console.log('getting names from DB...')
+    axios
+      .get('http://localhost:3001/db')
+      .then(response => {
+        //console.log("DB response :: ", response.data)
+        setPersons(response.data.persons)
+      })
+  }, [])
+  
   return (
     <div>
       <h2>Phonebook</h2>
