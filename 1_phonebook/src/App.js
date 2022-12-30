69ff7dd47613fe49a878348d7e8ef31148a12150
unknown <souporcerealemail@gmail.com>
2022-12-29 21:29:32 -0500

    2.16+2.17

--- 1_phonebook/src/App.js
+++ 1_phonebook/src/App.js
@@ -3,9 +3,9 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import axios from 'axios'
-import names from './services/names'
+import phonebook from './services/phonebook'
 
-const App = () => {
+const App = () => { 
   const [persons, setPersons] = useState([
     { name: 'Loading', number: '...', id: -1 }
   ]) 
@@ -18,7 +18,7 @@ const App = () => {
     if(persons.find(person => person.name === newName)){
       alert(`${newName} is already added to phonebook`);
     }else{
-      names.create({ name: newName, number: newNumber })
+      phonebook.create({ name: newName, number: newNumber })
       .then(returnedNote => {
         setPersons(persons.concat(returnedNote))
         setNewName("")
@@ -39,8 +39,22 @@ const App = () => {
     setNewSearch(event.target.value)
   }
 
+  const handleDeleteButton = (event) => {
+    event.preventDefault()
+    let deleteName = event.target.name
+    let deleteID = event.target.id
+    if (window.confirm(`Delete ${deleteName} ?`)) {
+      phonebook.del({ delID: deleteID })
+      .then(returnedNote => {
+        setPersons(
+          persons.filter(person=>person.id != deleteID)
+        )
+      }).catch(err=>console.log("Error deleting person! ", err))
+    }
+  }
+
   useEffect(() => {
-    names.getAll().then(allNames => {
+    phonebook.getAll().then(allNames => {
       setPersons(allNames)
     })
   }, [])
@@ -52,7 +66,7 @@ const App = () => {
       <h2>add a new</h2>
       <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <Persons persons={persons} newSearch={newSearch} />
+      <Persons persons={persons} newSearch={newSearch} handleDeleteButton={handleDeleteButton} />
     </div>
   )
 }
